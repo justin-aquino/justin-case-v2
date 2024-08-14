1859a71d5dd04d4be0acdade2b682877e3421d61
justin-aquino <justin.aquino25@gmail.com>
2024-08-13 20:18:07 -0700

    adding project list cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Button } from "react-bootstrap";
 import "./App.css";
-import Footer from "./layouts/Footer/Footer";
-import NaviBar from "./layouts/Navibar/Navibar";
+import Footer from "./Components/layouts/Footer/Footer";
+import NaviBar from "./Components/layouts/Navibar/Navibar";
+import TechStack from "./Components/Pages/TechStack/TechStack";
+import ProjectList from "./Components/Pages/Portfolio/ProjectList";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,25 +13,21 @@ function App() {
       <div>
         <NaviBar />
         <h1>test</h1>
+        <div className="flex-with-center" id="tech-stack">
+          <TechStack />
+        </div>
+
+        <div id="project">
+          <ProjectList />
+        </div>
         {/* 
         <Landing />
         <div className="flex-with-center" id="tech-stack">
           <TechStack />
         </div>
         <About />
-        <div id="project">
-          <ProjectList projects={projects} />
-        </div>
+        
          */}
-        <Button variant="primary">Primary</Button>{" "}
-        <Button variant="secondary">Secondary</Button>{" "}
-        <Button variant="success">Success</Button>{" "}
-        <Button variant="warning">Warning</Button>{" "}
-        <Button variant="danger">Danger</Button>{" "}
-        <Button variant="info">Info</Button>{" "}
-        <Button variant="light">Light</Button>{" "}
-        <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
         <Footer />
       </div>
     </>
